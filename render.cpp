215251b2c01d4df71a0717b07490f175e169eeaa
Eric <ericjw@vt.edu>
2017-04-27 00:21:28 -0400

    shading (dot product) works, need to work on actual intersection shadows

--- render.cpp
+++ render.cpp
@@ -1,8 +1,7 @@
 #include "render.hpp"
 #include <QColor>
 #include <QImage>
-
-
+#include <QDebug>
 
 RayTracer::RayTracer(std::vector<Light> li, Camera ca)
 {
@@ -13,11 +12,11 @@ RayTracer::RayTracer(std::vector<Light> li, Camera ca)
 
 void RayTracer::render(const std::vector<std::unique_ptr<Object>> &objects, const std::string &fname) {
 	QImage img(cam.size[1], cam.size[0], QImage::Format_RGB32);
-	//std::vector<std::vector<Vect>> pixelsX;
+	std::vector<std::vector<Vect>> pixelsX;
 
 	Vect orig = Vect(cam.center.x, cam.center.y, cam.center.z) - (Vect(cam.normal.x, cam.normal.y, cam.normal.z) * cam.focus);
 	for (int x = 0; x < cam.size[1]; x++) {
-		//std::vector<Vect> pixelsY;
+		std::vector<Vect> pixelsY;
 		for (int y = 0; y < cam.size[0]; y++) {
 
 			double i = cam.resolution[1] * (x - (cam.size[1]/2));
@@ -25,22 +24,70 @@ void RayTracer::render(const std::vector<std::unique_ptr<Object>> &objects, cons
 
 			Vect dir = norm(Vect(i,j,0) - orig);
 			Vect tmp = castRay(orig, dir, objects);
-			//pixelsY.push_back(tmp);
-			img.setPixel(x, y, QColor(tmp.x, tmp.y, tmp.z).rgb());
+			pixelsY.push_back(tmp);
+			//img.setPixel(x, y, QColor(tmp.x, tmp.y, tmp.z).rgb());
 		}
-		//pixelsX.push_back(pixelsY);
+		pixelsX.push_back(pixelsY);
 	}
 
-	/*int x = 0;
+	double max = 0;
+	for (auto vx : pixelsX) {
+		for (auto vy : vx) {
+
+			if (vy.x > max) {
+				max = vy.x;
+			}
+			else if (vy.y > max) {
+				max = vy.y;
+			}
+			else if (vy.z > max) {
+				max = vy.z;
+			}
+		}
+	}
+
+	for (std::vector<Vect> &vx : pixelsX) {
+		for (Vect &vy : vx) {
+			double tmpScale = (255. / max);
+			vy = vy * tmpScale;
+		}
+	}
+
+	//double newmax = 0;
+	//for (auto vx : pixelsX) {
+	//	for (auto vy : vx) {
+	//		if (vy.x > newmax) {
+	//			newmax = vy.x;
+	//		}
+	//		else if (vy.y > newmax) {
+	//			newmax = vy.y;
+	//		}
+	//		else if (vy.z > newmax) {
+	//			newmax = vy.z;
+	//		}
+	//	}
+	//}
+
+	int x = 0;
 	int y = 0;
 	for (auto vx : pixelsX) {
 		for (auto vy : vx) {
-			img.setPixel(x, y, QColor(vy.x, vy.y, vy.z).rgb());
+			img.setPixel(x, y, QColor((int)vy.x, (int)vy.y, (int)vy.z).rgb());
 			y++;
 		}
 		x++;
 		y = 0;
-	}*/
+	}
+
+	//for (auto vx : pixelsX) {
+	//	for (auto vy : vx) {
+	//		if (vy.x != 0 || vy.y != 0 ||vy.z != 0)
+	//			qDebug() << "x " << vy.x << " y " << vy.y << " z " << vy.z;
+	//	}
+
+	//}
+
+	qDebug() << "max " << max; // << " newmax " << newmax;
 
 	img.save(fname.c_str());
 }
@@ -70,17 +117,20 @@ Vect RayTracer::castRay (const Vect &orig, const Vect &dir, const std::vector<st
 
 		Vect point = orig + dir * dist;
 		Vect nhit = norm(point - Vect(hitObject->getCenter()));
-		Vect shadow_ray(Vect(lights.at(0).location.x, lights.at(0).location.y, lights.at(0).location.z));
 
 		for (auto l : lights) {
+			Vect shadow_ray(Vect(l.location.x, l.location.y, l.location.z) - point);
 			//if not shadowed
-			if (true) {
+			if (true) { //!trace(point, shadow_ray, objects, dist, hitObject)) {
 				double scale = dot(nhit, shadow_ray) * hitObject->getLambert();
+				if (scale < 0) {
+					scale = 0;
+				}
 				totalCol = totalCol + (col * scale * l.intensity);
 			}
 
 		}
-		return col;
+		return totalCol;
 	}
 
 	return Vect(0, 0, 0);
